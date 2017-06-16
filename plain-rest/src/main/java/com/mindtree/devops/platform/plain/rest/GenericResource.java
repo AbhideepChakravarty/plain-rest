a38109dad3e381368ce954e139df0412ff6323bf
M1011280 <M1011280@A2ML11092.mindtree.com>
2017-06-16 17:03:33 +0530

    Adding code to plain-rest API

---
+++ plain-rest/src/main/java/com/mindtree/devops/platform/plain/rest/GenericResource.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mindtree.devops.platform.plain.rest;
+
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Produces;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * REST Web Service
+ *
+ * @author M1011280
+ */
+@Path("rest")
+public class GenericResource {
+
+    @Context
+    private UriInfo context;
+
+    /**
+     * Creates a new instance of GenericResource
+     */
+    public GenericResource() {
+    }
+
+    /**
+     * Retrieves representation of an instance of com.mindtree.devops.platform.plain.rest.GenericResource
+     * @return an instance of java.lang.String
+     */
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getXml() {
+        //TODO return proper representation object
+        return "hello";
+    }
+
+    /**
+     * PUT method for updating or creating an instance of GenericResource
+     * @param content representation for the resource
+     */
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public void putXml(String content) {
+        System.out.println("OUt put --- " + content);
+    }
+}
